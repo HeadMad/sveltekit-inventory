92079780cacd351b75b20e2a8785045a227515a7
HeadMad <kov.ps@ya.ru>
2023-06-17 21:12:47 +0800

    fix upload sheets

--- src/routes/catalog/api/+server.js
+++ src/routes/catalog/api/+server.js
@@ -12,13 +12,12 @@
 
 import db from "$lib/sqlite/tempDb.js";
 import {createHandlerResponse} from "$lib/api";
+import {json} from '@sveltejs/kit';
 
-export async function post({ request }) {
+export async function POST({ request }) {
   const {method, params} = await request.json();
   const response = await createHandlerResponse(db[method])(params);
   
-  return {
-    body: response
-  }
+  return json(response);
 }
 
